33534b05be6902e1e634bd6976a9fc9772f37ced
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-01-03 22:03:57 +0100

    Configuracion de fuentes

---
+++ src/config/fonsts.ts
@@ -0,0 +1,10 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+export const titleFont = Montserrat_Alternates({
+    subsets: ['latin'],
+    weight: ['500', '700'],
+});
+
+export const baseFont = Inter({ 
+    subsets: ['latin'] 
+});
---
+++ src/interfaces/interfaces.d.ts
@@ -0,0 +1,3 @@
+export interface LayoutProps {
+    readonly children?: React.ReactNode;
+}
